d030c32cce11a8637470dc943682bd5f5c3f2827
SebasDeveloper <johann.martinez@carvajal.com>
2014-10-16 15:53:27 -0500

    crear File

---
+++ DocumentManagement.Provider/DocumentManagement.Provider/Controller/File.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Provider.Controller
+{
+    static class File
+    {
+        public string LoadFile(string Url)
+        {
+
+            return null;
+        }    
+    }
+}
