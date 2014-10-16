8f4fbd833795c101068faba7ce304901e38ded32
SebasDeveloper <johann.martinez@carvajal.com>
2014-10-16 18:50:49 -0500

    File Upload

--- DocumentManagement.Provider/DocumentManagement.Provider/Models/Constants.cs
+++ DocumentManagement.Provider/DocumentManagement.Provider/Models/Constants.cs
@@ -9,5 +9,9 @@ namespace DocumentManagement.Provider.Models
     public class Constants
     {
         public const string P_ProviderConnectionName = "DM_ProviderConnection";
+
+        public const string C_Settings_File_FileModuleName = "S3_ProveedoresOnline";
+        
+        public const string C_Settings_File_ProviderFile_RemoteFolder = "ProviderFile_RemoteFolder";
     }
 }
