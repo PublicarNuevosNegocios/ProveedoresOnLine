ce226f960c7fee1ae1ba4c9234f99df797d897fb
jairoguzman <jairo.guzman@carvajal.com>
2014-10-17 15:41:40 -0500

    funciones de crear customer.

--- DocumentManagement/DocumentManagement.Web/Controllers/CustomerController.cs
+++ DocumentManagement/DocumentManagement.Web/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using DocumentManagement.Models.Customer;
+﻿using DocumentManagement.Customer.Models.Customer;
+using DocumentManagement.Models.Customer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,10 +20,20 @@ namespace DocumentManagement.Web.Controllers
             UpserCustomerModel oModel = new UpserCustomerModel()
             {
                 CustomerOptions = DocumentManagement.Customer.Controller.Customer.CatalogGetCustomerOptions(),
+                RelatedCustomer = new CustomerModel() { CustomerPublicId = CustomerPublicId },
             };
 
-            if (!string.IsNullOrEmpty(CustomerPublicId))
-                oModel.RelatedCustomer = DocumentManagement.Customer.Controller.Customer.CustomerGetById(CustomerPublicId);
+            if (!string.IsNullOrEmpty(Request["UpsertAction"]) && Request["UpsertAction"].Trim() == "true")
+            {
+                oModel.RelatedCustomer = GetCustomerRequest();
+
+                oModel.RelatedCustomer.CustomerPublicId = DocumentManagement.Customer.Controller.Customer.CustomerUpsert(oModel.RelatedCustomer);
+
+                return RedirectToAction(MVC.Customer.ActionNames.UpsertCustomer, MVC.Customer.Name, new { CustomerPublicId = oModel.RelatedCustomer.CustomerPublicId });
+            }
+
+            if (!string.IsNullOrEmpty(oModel.RelatedCustomer.CustomerPublicId))
+                oModel.RelatedCustomer = DocumentManagement.Customer.Controller.Customer.CustomerGetById(oModel.RelatedCustomer.CustomerPublicId);
 
             return View(oModel);
         }
@@ -46,5 +57,24 @@ namespace DocumentManagement.Web.Controllers
 
             return View(oModel);
         }
+
+        #region private methods
+
+        private CustomerModel GetCustomerRequest()
+        {
+            CustomerModel oReturn = new CustomerModel();
+
+            if (!string.IsNullOrEmpty(Request["UpsertAction"]) && Request["UpsertAction"].Trim() == "true")
+            {
+                oReturn.CustomerPublicId = Request["CustomerPublicId"];
+                oReturn.Name = Request["Name"];
+                oReturn.IdentificationType = new Customer.Models.Util.CatalogModel() { ItemId = Convert.ToInt32(Request["IdentificationType"]), };
+                oReturn.IdentificationNumber = Request["IdentificationNumber"];
+            }
+
+            return oReturn;
+        }
+
+        #endregion
     }
 }
