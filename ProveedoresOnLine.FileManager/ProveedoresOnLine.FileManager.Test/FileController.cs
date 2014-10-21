1e5423e2a87783c15c9c3a1a44a0db2cd8a25e73
jairoguzman <jairo.guzman@carvajal.com>
2014-10-21 12:26:49 -0500

    Implementacion independiente de archivos.

---
+++ ProveedoresOnLine.FileManager/ProveedoresOnLine.FileManager.Test/FileController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProveedoresOnLine.FileManager.Test
+{
+    [TestClass]
+    public class FileController
+    {
+        [TestMethod]
+        public void LoadFile()
+        {
+            string FilePath = @"D:\Proyectos\Github\ProveedoresOnLine\ProveedoresOnLine.FileManager\ProveedoresOnLine.FileManager.Test\Desert.jpg";
+
+            string oReturn = ProveedoresOnLine.FileManager.FileController.LoadFile(FilePath, "\\tmp\\");
+
+            Assert.AreEqual(true, !string.IsNullOrEmpty(oReturn));
+        }
+    }
+}
---
+++ ProveedoresOnLine.FileManager/ProveedoresOnLine.FileManager/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.FileManager
+{
+    internal class Constants
+    {
+        internal const string C_Settings_File_FileModuleName = "S3_ProveedoresOnLine";
+    }
+}
