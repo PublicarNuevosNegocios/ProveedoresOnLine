016b5f31a2a0ff1797a786befe3bccbdfb651844
jairoguzman <jairo.guzman@carvajal.com>
2014-10-22 17:07:57 -0500

    Ajuste botones de formulario.

--- DocumentManagement/DocumentManagement.Web/Scripts/Site/ProviderFormScripts.js
+++ DocumentManagement/DocumentManagement.Web/Scripts/Site/ProviderFormScripts.js
@@ -1,4 +1,11 @@
-﻿//init spiner
+﻿//postback form
+function PF_PostBackForm(vidForm, NewStepId) {
+    var strUrl = $("#" + vidForm).attr('action').replace(/{{NewStepId}}/, NewStepId);
+    $("#" + vidForm).attr('action', strUrl);
+    $("#" + vidForm).submit();
+}
+
+//init spiner
 function PF_InitSpinner(vidDiv) {
     $("#" + vidDiv).spinner();
 }
