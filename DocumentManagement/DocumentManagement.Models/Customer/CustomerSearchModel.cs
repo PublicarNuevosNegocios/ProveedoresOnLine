b9dbbdfdec68771fc95477c8c117779f8940f16a
jairoguzman <jairo.guzman@carvajal.com>
2014-10-22 20:54:03 -0500

    Ajusta modelos y referencias.

--- DocumentManagement/DocumentManagement.Models/Customer/CustomerSearchModel.cs
+++ DocumentManagement/DocumentManagement.Models/Customer/CustomerSearchModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DocumentManagement.Models.Provider
+namespace DocumentManagement.Models.Customer
 {
     public class CustomerSearchModel
     {
