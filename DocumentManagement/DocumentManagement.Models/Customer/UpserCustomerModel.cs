39a077f16061f42e93fa280220fabeccd4147500
SebasDeveloper <johann.martinez@carvajal.com>
2014-10-22 19:47:10 -0500

    Funcionalidad de la pantalla de las URL

--- DocumentManagement/DocumentManagement.Models/Customer/UpserCustomerModel.cs
+++ DocumentManagement/DocumentManagement.Models/Customer/UpserCustomerModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DocumentManagement.Models.Customer
+namespace DocumentManagement.Models.Provider
 {
     public class UpserCustomerModel
     {
--- DocumentManagement/DocumentManagement.Models/Provider/ProviderSearchModel.cs
+++ DocumentManagement/DocumentManagement.Models/Provider/ProviderSearchModel.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DocumentManagement.Models.Customer
+namespace DocumentManagement.Models.Provider
 {
     public class ProviderSearchModel
     {
         public int TotalRows { get; set; }
 
-        public List<DocumentManagement.Provider.Models.Provider.ProviderModel> RelatedProvider { get; set; }
+        public List<ProviderItemSearchModel> RelatedProvider { get; set; }
+
+        public List<DocumentManagement.Customer.Models.Customer.CustomerModel> Customers { get; set; }
+
+        public List<DocumentManagement.Customer.Models.Form.FormModel> Forms { get; set; }
     }
 }
