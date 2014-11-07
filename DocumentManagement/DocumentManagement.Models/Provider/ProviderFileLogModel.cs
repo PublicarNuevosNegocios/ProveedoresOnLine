f6c446c7c623312c150f5a400b54701e92bc21e5
Davidmt89 <d.alonsomt@gmail.com>
2014-11-07 18:09:19 -0500

    se agrega estructura de log file model

---
+++ DocumentManagement/DocumentManagement.Models/Provider/ProviderFileLogModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentManagement.Models.Provider
+{
+    public class ProviderFileLogModel
+    {
+        public string Url { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string FieldType { get; set; }
+    }
+}
---
+++ DocumentManagement/DocumentManagement.Web/Controllers/LogFileController.cs
@@ -0,0 +1,26 @@
+﻿using DocumentManagement.Models.Provider;
+using DocumentManagement.Provider.Models.Provider;
+using DocumentManagement.Provider.Models.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocumentManagement.Web.Controllers
+{
+    public partial class LogFileController : BaseController
+    {
+        public virtual ActionResult Index()
+        {
+
+            //ProviderSearchModel oModel = new ProviderSearchModel();
+            //int oTotalRows;
+            //oModel.Customers = DocumentManagement.Customer.Controller.Customer.CustomerSearch(null, 0, 20, out oTotalRows);
+            ////oModel.Forms = DocumentManagement.Customer.Controller.Customer.FormSearch(null, 0, 20, out oTotalRows);
+            //return View(oModel);
+            return View();
+        }
+    }
+}
