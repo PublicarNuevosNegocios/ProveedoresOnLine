1df258b3b7839c2a78a286b96ce9adaf48b30fcc
jairoguzman <jairo.guzman@carvajal.com>
2014-11-13 10:52:35 -0500

    Crea proyecto de crawler.

---
+++ Crawler/Crawler.Manager/CrawlerManager.cs
@@ -0,0 +1,32 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.Manager
+{
+    public static class CrawlerManager
+    {
+        public static void CrawlerBasicInfo(string ParProviderId)
+        {
+            string oParUrl = Crawler.Manager.Models.InternalSettings.Instance
+                [Crawler.Manager.Models.Constants.C_Settings_CrawlerUrl_BasicInfo].
+                Value.
+                Replace("{{ParProviderId}}", ParProviderId);
+
+            string strHtml = (new System.Net.WebClient()).DownloadString(oParUrl);
+
+            HtmlDocument HtmlDoc = new HtmlDocument();
+            HtmlDoc.LoadHtml(strHtml);
+
+            var xxx = HtmlDoc.DocumentNode.SelectNodes("//a[@href]");
+
+            //xxx[0].Attributes["href"].Value;
+
+            //(new System.Net.WebClient()).DownloadFile(oParUrl,@"D:\Proyectos\Github\ProveedoresOnLine\Crawler\Crawler\1.html");
+
+        }
+    }
+}
---
+++ Crawler/Crawler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //crawl basic info
+            Crawler.Manager.CrawlerManager.CrawlerBasicInfo("1733");
+
+        }
+    }
+}
