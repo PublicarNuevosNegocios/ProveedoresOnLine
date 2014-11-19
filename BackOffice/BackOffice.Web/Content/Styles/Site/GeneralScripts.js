a4d6a46f55229e19e93f156287e92eee90688fc9
jairoguzman <jairo.guzman@carvajal.com>
2014-11-19 17:17:17 -0500

    Crea backoffice de proveedores on line.

---
+++ BackOffice/BackOffice.Web/Content/Styles/Site/GeneralScripts.js
@@ -0,0 +1,9 @@
+﻿/*init base url*/
+var BaseUrl = {
+    ApiUrl: '',
+    SiteUrl: '',
+    Init: function (vInitObject) {
+        this.ApiUrl = vInitObject.ApiUrl;
+        this.SiteUrl = vInitObject.SiteUrl;
+    },
+};
---
+++ BackOffice/BackOffice.Web/Global.asax.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BackOffice.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected static bool EnableSSL
+        {
+            get
+            {
+                try
+                {
+                    return Convert.ToBoolean
+                        (System.Configuration.ConfigurationManager.
+                            AppSettings["EnableSSL"].
+                            ToLower().
+                            Replace(" ", ""));
+                }
+                catch
+                {
+                    return true;
+                }
+            }
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (EnableSSL)
+            {
+                //ensure only https navigation
+                if (!Context.Request.IsSecureConnection)
+                    Response.Redirect(Context.Request.Url.ToString().Replace("http:", "https:"), true);
+            }
+        }
+
+        #region Enable web api session read
+
+        private const string _WebApiPrefix = "api";
+        private static string _WebApiExecutionPath = String.Format("~/{0}", _WebApiPrefix);
+
+        protected void Application_PostAuthorizeRequest()
+        {
+            if (IsWebApiRequest())
+            {
+                HttpContext.Current.SetSessionStateBehavior(System.Web.SessionState.SessionStateBehavior.Required);
+            }
+        }
+        private static bool IsWebApiRequest()
+        {
+            return HttpContext.Current.Request.AppRelativeCurrentExecutionFilePath.StartsWith(_WebApiExecutionPath);
+        }
+
+        #endregion
+    }
+}
