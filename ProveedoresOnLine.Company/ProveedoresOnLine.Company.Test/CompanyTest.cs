7e80cb5021b07f565658b1fc5a4bf4335ed18fa4
jairoguzman <jairo.guzman@carvajal.com>
2014-11-20 14:35:50 -0500

    Ajuste logs.

---
+++ ProveedoresOnLine.Company/ProveedoresOnLine.Company.Test/CompanyTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProveedoresOnLine.Company.Test
+{
+    [TestClass]
+    public class CompanyTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ProveedoresOnLine.Company.Controller.GenericLog.GetGenericLogModel();
+
+        }
+    }
+}
