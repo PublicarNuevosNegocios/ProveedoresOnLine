560fcbf586cb3cbfaa8848b87f31c028f1ee70a7
SebasDeveloper <johann.martinez@carvajal.com>
2014-12-22 08:24:32 -0500

    AdminController

---
+++ BackOffice/BackOffice.Web/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackOffice.Web.Controllers
+{
+    public partial class AdminController : BaseController
+    {
+        public virtual ActionResult Index()
+        {
+            return View();
+        }
+
+        public virtual ActionResult LogOutUser()
+        {         
+            return RedirectToAction(MVC.Home.ActionNames.Index, MVC.Home.Name);
+        }
+    }
+}
