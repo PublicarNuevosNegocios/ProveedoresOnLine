2b8b996b9541e5817da935d26008e3cf5733b5c8
SebasDeveloper <johann.martinez@carvajal.com>
2015-01-22 12:40:07 -0500

    ProviderViewModel

---
+++ MarketPlace/MarketPlace.Models/Provider/ProviderViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Models.Provider
+{
+    public class ProviderViewModel
+    {
+        public bool RenderScripts { get; set; }
+
+        public List<ProveedoresOnLine.Company.Models.Util.CatalogModel> ProviderOptions { get; set; }
+
+        public List<ProviderLiteViewModel> RelatedProvider { get; set; }
+
+        public List<MarketPlace.Models.General.GenericMenu> ProviderMenu { get; set; }
+
+        public MarketPlace.Models.General.GenericMenu CurrentSubMenu
+        {
+            get
+            {
+                if (ProviderMenu != null)
+                {
+                    return ProviderMenu.
+                                Where(x => x.ChildMenu.Any(y => y.IsSelected && !string.IsNullOrEmpty(y.Url))).
+                                Select(x => x.ChildMenu.
+                                    Where(y => y.IsSelected && !string.IsNullOrEmpty(y.Url)).
+                                    FirstOrDefault()).
+                                FirstOrDefault();
+                }
+                return null;
+            }
+        }        
+    }
+}
