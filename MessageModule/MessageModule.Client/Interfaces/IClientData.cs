6a814c1808aa28ed9231c600447005c36e67c26e
jairoguzman <jairo.guzman@carvajal.com>
2015-02-10 14:05:45 -0500

    Crea modulo de mensajeria y dll de cliente.

---
+++ MessageModule/MessageModule.Client/Interfaces/IClientData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageModule.Client.Interfaces
+{
+    internal interface IClientData
+    {
+        int MessageQueueCreate(string Agent, DateTime ProgramTime, string User);
+
+        void MessageQueueInfoCreate(int MessageQueueId, string Parameter, string Value);
+    }
+}
