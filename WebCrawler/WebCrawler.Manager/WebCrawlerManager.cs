9a693183204afa8485700e5567904ca9c4934ea1
Davidmt89 <d.alonsomt@gmail.com>
2015-02-11 09:33:53 -0500

    se creó la solución WebCrawler encargada de obtener la información de los proveedores que va a ser relacionada en BackOffice

---
+++ WebCrawler/WebCrawler.Manager/WebCrawlerManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler.Manager
+{
+    public static class WebCrawlerManager
+    {
+        public static void WebCrawlerInfo()
+        {
+
+        }
+    }
+}
