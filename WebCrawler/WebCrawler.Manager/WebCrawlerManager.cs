e38a1eb77a435960d0b9a7c8e6632e5c4f712775
Davidmt89 <d.alonsomt@gmail.com>
2015-02-11 18:04:33 -0500

    se modificó la lógica para obtener los valores de la pantalla de información general del proveedor.

--- WebCrawler/WebCrawler.Manager/WebCrawlerManager.cs
+++ WebCrawler/WebCrawler.Manager/WebCrawlerManager.cs
@@ -36,13 +36,75 @@ namespace WebCrawler.Manager
                     Value.
                     Replace("{{ParProviderId}}", ParId);
 
-                Console.WriteLine("\n Download " + settings);
+                Console.WriteLine("\n Descargando Información de " + settings);
 
                 string strHtml = oWebClient.DownloadString(oParURL);
 
                 HtmlDocument HtmlDoc = new HtmlDocument();
                 HtmlDoc.LoadHtml(strHtml);
+
+                if (settings == enumMenu.GeneralInfo.ToString())
+                {
+                    GetGeneralInfo(HtmlDoc);
+                }
             }
+        }
+
+        #region General Info
+
+        public static void GetGeneralInfo(HtmlDocument HtmlDoc)
+        {
+            if (HtmlDoc.DocumentNode.SelectNodes("//table[@class='administrador_tabla_generales']") != null)
+            {
+                HtmlNodeCollection table = HtmlDoc.DocumentNode.SelectNodes("//table[@class='administrador_tabla_generales']");
+                HtmlNodeCollection cells = table[0].SelectNodes("//tr/td");
+                string a = string.Empty;
+                foreach (var c in cells)
+                {
+                    HtmlAttribute att = c.Attributes["input"];
+                    if (att != null)
+                    {
+                        if (att.Value.Contains("nit2"))
+                        {
+                            a += att.Value.ToString() + "\n";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("la sección " + enumMenu.GeneralInfo.ToString() + " no tiene información para descargar." + "\n");
+            }
+        }
+
+        #endregion
+
+        public static string UploadFile(
+            string ProviderPublicId,
+            string UrlFile,
+            string UrlNewFile)
+        {
+            string strRemoteFile = string.Empty;
+
+            string strFile = string.Empty;
+
+            strFile = UrlNewFile +
+                "\\ProviderFile_" +
+                ProviderPublicId + "_" +
+                "0" + "_" +
+                DateTime.Now.ToString("yyyyMMddHHmmss") + ".pdf";
+            File.Copy(UrlFile, strFile);
+
+            //load file to s3
+            strRemoteFile = ProveedoresOnLine.FileManager.FileController.LoadFile
+                        (strFile,
+                        WebCrawler.Manager.General.InternalSettings.Instance
+                        [WebCrawler.Manager.General.Constants.C_Settings_File_RemoteDirectoryProvider].Value +
+                            ProviderPublicId + "\\");
+
+            File.Delete(strFile);
+
+            return strRemoteFile;
         }
 
         public class MyWebClient : System.Net.WebClient
