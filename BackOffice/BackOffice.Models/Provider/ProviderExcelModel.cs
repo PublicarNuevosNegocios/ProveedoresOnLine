d5532ed19687efa8bb285ae49c62087c3a3e63d2
SebasDeveloper <johann.martinez@carvajal.com>
2015-02-16 12:09:32 -0500

    Funcionalidad de carga de proveedores en listas restrictivas

--- BackOffice/BackOffice.Models/Provider/ProviderExcelModel.cs
+++ BackOffice/BackOffice.Models/Provider/ProviderExcelModel.cs
@@ -8,11 +8,12 @@ namespace BackOffice.Models.Provider
 {
     public class ProviderExcelModel
     {
-        public string RazonSocial { get; set; }
-        public string IdentificationType { get; set; }
-        public string IdentifiationNumber { get; set; }
-        public string SearchType { get; set; }   
         public string ProviderPublicId { get; set; }
-        public string BlackListSatatus { get; set; }        
+        public string BlackListStatus { get; set; }     
+
+        //public string RazonSocial { get; set; }
+        //public string IdentificationType { get; set; }
+        //public string IdentifiationNumber { get; set; }
+        //public string SearchType { get; set; }            
     }
 }
