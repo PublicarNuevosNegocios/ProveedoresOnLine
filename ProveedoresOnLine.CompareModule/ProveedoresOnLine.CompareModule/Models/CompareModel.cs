5d7ac298df1f430e8493004371d95d56b6ef3e58
jairoguzman <jairo.guzman@carvajal.com>
2015-02-20 18:18:29 -0500

    Crea crud de modulo de comparacion.

---
+++ ProveedoresOnLine.CompareModule/ProveedoresOnLine.CompareModule/Models/CompareModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.CompareModule.Models
+{
+    public class CompareModel
+    {
+        public int CompareId { get; set; }
+
+        public string CompareName { get; set; }
+
+        public string User { get; set; }
+
+        public bool Enable { get; set; }
+
+        public DateTime LastModify { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public List<CompareCompanyModel> RelatedProvider { get; set; }
+    }
+}
---
+++ ProveedoresOnLine.CompareModule/ProveedoresOnLine.CompareModule/Models/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.CompareModule.Models
+{
+    public class Constants
+    {
+        public const string C_POL_CompareConnectionName = "POL_CompareConnection";
+    }
+}
