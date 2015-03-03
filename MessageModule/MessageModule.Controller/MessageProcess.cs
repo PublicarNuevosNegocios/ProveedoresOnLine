98d8d56a9c934473430faff97c5b8d5bca1b704a
jairoguzman <jairo.guzman@carvajal.com>
2015-03-03 14:43:48 -0500

    Avance modulo de mensajeria.

---
+++ MessageModule/MessageModule.Controller/MessageProcess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageModule.Controller
+{
+    public class MessageProcess
+    {
+        public static void StartProcess()
+        {
+            //get message to send
+            List<MessageModule.Interfaces.Models.MessageModel> lstMessageToSend =
+                MessageModule.DAL.Controller.MessageDataController.Instance.MessageQueueGetMessageToProcess();
+
+            if (lstMessageToSend != null && lstMessageToSend.Count > 0)
+            {
+                lstMessageToSend.All(mts => {
+
+                    //get agent configuration
+
+
+
+                    return true;
+                });
+                
+            }
+        }
+    }
+}
--- MessageModule/MessageModule.Interfaces/General/Constants.cs
+++ MessageModule/MessageModule.Interfaces/General/Constants.cs
@@ -3,6 +3,16 @@
     public class Constants
     {
         public const string C_MS_MessageConnection = "MS_MessageConnection";
+
+        #region Internal Settings
+
+        public const string C_SettingsModuleName = "MessageModule";
+
+        public const string C_Settings_Agent = "Agent";
+
+        public const string C_Settings_AgentConfig = "AgentConfig_{AgentName}";
+
+        #endregion
     }
 }
 
