66dcdfafb2554420df241c51063112705b3154ba
jairoguzman <jairo.guzman@carvajal.com>
2015-03-04 19:10:22 -0500

    Crea servicio de quartz para procesos de proveedores on line.

---
+++ ProveedoresOnLine.Process/ProveedoresOnLine.Process.Test/MessageModuleJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProveedoresOnLine.Process.Test
+{
+    [TestClass]
+    public class MessageModuleJob
+    {
+        [TestMethod]
+        public void MessageModuleJob_Execute()
+        {
+            ProveedoresOnLine.Process.Implement.MessageModuleJob MMJOb = new Implement.MessageModuleJob();
+            MMJOb.Execute(null);
+        }
+    }
+}
