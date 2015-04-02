fdd2462cfe782e59129863d98ec3cd068b8242b6
jairoguzman <jairo.guzman@carvajal.com>
2015-04-01 19:01:05 -0500

    Avance upsert survey.

--- MarketPlace/MarketPlace.Web/Controllers/SurveyController.cs
+++ MarketPlace/MarketPlace.Web/Controllers/SurveyController.cs
@@ -53,6 +53,64 @@ namespace MarketPlace.Web.Controllers
 
         public virtual ActionResult SurveyUpsert(string SurveyPublicId, string StepId)
         {
+            //get current survey values
+            MarketPlace.Models.Survey.SurveyViewModel oSurvey =
+               new Models.Survey.SurveyViewModel(ProveedoresOnLine.SurveyModule.Controller.SurveyModule.SurveyGetById(SurveyPublicId));
+
+            //loop request for update answers
+            Request.Form.AllKeys.Where(x => x.Contains("SurveyItem_")).All(req =>
+            {
+                string[] strAux = Request[req].Split('_');
+
+                if (strAux.Length >= 2)
+                {
+                    int oSurveyConfigItemId = Convert.ToInt32(strAux[1].Replace(" ", ""));
+
+                    MarketPlace.Models.Survey.SurveyItemViewModel oSurveyItem = oSurvey.GetSurveyItem(oSurveyConfigItemId);
+
+                    if (oSurveyItem == null)
+                    {
+                        oSurveyItem = new Models.Survey.SurveyItemViewModel(new ProveedoresOnLine.SurveyModule.Models.SurveyItemModel()
+                        {
+                            RelatedSurveyConfigItem = new ProveedoresOnLine.Company.Models.Util.GenericItemModel()
+                            {
+                                ItemId = oSurveyConfigItemId,
+                            },
+                            Enable = true,
+                        });
+                    }
+
+                    if (oSurveyItem.RelatedSurveyItem.ItemInfo == null)
+                    {
+                        oSurveyItem.RelatedSurveyItem.ItemInfo = new List<ProveedoresOnLine.Company.Models.Util.GenericItemInfoModel>();
+                    }
+
+
+
+                }
+
+                return true;
+            });
+
+
+            ////eval each survey config item
+            //var lstEvaluationArea = oSurvey.GetSurveyConfigItem(enumSurveyConfigItemType.EvaluationArea, null);
+
+            //lstEvaluationArea.All(ea =>
+            //{
+            //    if(ea.)
+
+
+
+            //    return true;
+            //});
+
+
+
+
+
+
+
             return RedirectToRoute
                 (MarketPlace.Models.General.Constants.C_Routes_Default,
                 new
