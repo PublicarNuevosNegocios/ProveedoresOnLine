af2ac7605d77497146b9a757b16dedfe70c4110b
SebasDeveloper <johann.martinez@carvajal.com>
2015-04-08 17:59:20 -0500

    Funcion de Batch de K de contratación. Pendiente identificar el role

---
+++ ProveedoresOnLine.CompanyProvider/ProveedoresOnLine.CompanyProviderBatch/Models/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.CompanyProviderBatch.Models
+{
+    public class Constants
+    {
+        #region AppSettings
+
+        public const string C_AppSettings_LogFile = "LogFile";
+
+        #endregion
+    }
+}
