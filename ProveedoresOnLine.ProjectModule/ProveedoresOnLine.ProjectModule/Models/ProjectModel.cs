979dad9ec87b494be28956b667b769ba29d1c290
jairoguzman <jairo.guzman@carvajal.com>
2015-04-10 15:02:13 -0500

    Agrega modelos de mp.

---
+++ ProveedoresOnLine.ProjectModule/ProveedoresOnLine.ProjectModule/Models/ProjectModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.ProjectModule.Models
+{
+    public class ProjectModel
+    {
+        public string ProjectPublicId { get; set; }
+
+        public string ProjectName { get; set; }
+
+        public ProjectConfigModel RelatedProjectConfig { get; set; }
+
+        public bool Enable { get; set; }
+
+        public DateTime LastModify { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+
+        public List<ProveedoresOnLine.Company.Models.Util.GenericItemInfoModel> ProjectInfo { get; set; }
+
+        public List<ProjectProviderModel> RelatedProjectProvider { get; set; }
+    }
+}
---
+++ ProveedoresOnLine.ProjectModule/ProveedoresOnLine.ProjectModule/Models/ProjectProviderInfoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.ProjectModule.Models
+{
+    public class ProjectProviderInfoModel : ProveedoresOnLine.Company.Models.Util.GenericItemInfoModel
+    {
+        public ProveedoresOnLine.Company.Models.Util.GenericItemModel RelatedEvaluationItem { get; set; }
+    }
+}
