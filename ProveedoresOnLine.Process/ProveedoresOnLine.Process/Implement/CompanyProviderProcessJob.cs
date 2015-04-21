5870c0bde93f4186f0dbba6a0ab57815792d01f0
SebasDeveloper <johann.martinez@carvajal.com>
2015-04-20 19:10:57 -0500

    CompanyProvider Batch

---
+++ ProveedoresOnLine.Process/ProveedoresOnLine.Process/Implement/CompanyProviderProcessJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.Process.Implement
+{
+    public class CompanyProviderProcessJob : Quartz.IJob
+    {
+        public void Execute(Quartz.IJobExecutionContext context)
+        {
+            ProveedoresOnLine.CompanyProviderBatch.RecruitmentProcess.StartProcess();
+        }
+    }
+}
