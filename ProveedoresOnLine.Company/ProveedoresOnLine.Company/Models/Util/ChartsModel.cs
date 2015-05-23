1f3424d1b7d7680e283f93a608e4b86c4416c9b4
SebasDeveloper <johann.martinez@carvajal.com>
2015-05-23 11:03:17 -0500

    Charts Model

---
+++ ProveedoresOnLine.Company/ProveedoresOnLine.Company/Models/Util/ChartsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.Company.Models.Util
+{
+    public class ChartsModel
+    {
+        public string Title { get; set; }
+
+        public int ItemType { get; set; }
+        
+        public string ItemName { get; set; }
+
+        public int Count { get; set; }
+
+        public string AxisX { get; set; }
+
+        public string AxisY { get; set; }
+    }
+}
+
