d58e1bfc39bb3aece68622c3e5797a2259db6b61
Alex-JP <gustavo.jurado@publicar.com>
2015-08-10 18:15:50 -0500

    Genrando Excel SurveyInformacionGeneral

--- MarketPlace/MarketPlace.Web/Controllers/ReportController.cs
+++ MarketPlace/MarketPlace.Web/Controllers/ReportController.cs
@@ -4,6 +4,7 @@ using MarketPlace.Models.Provider;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -42,6 +43,71 @@ namespace MarketPlace.Web.Controllers
             if (MarketPlace.Models.General.SessionModel.CurrentURL != null)
                 MarketPlace.Models.General.SessionModel.CurrentURL = null;
 
+
+            if (Request["SurveyGeneralInfoReport"] == "True")
+            {
+                byte[] buffer = null;
+                StringBuilder data = new StringBuilder();
+                List<ProveedoresOnLine.SurveyModule.Models.SurveyModel> surveyByProvider = ProveedoresOnLine.Reports.Controller.ReportModule.SurveyGetAllByCustomer(SessionModel.CurrentCompany.CompanyPublicId);
+                string strSep = ";";
+                data.AppendLine
+                        (
+                        "\"" + "Tipo Evaluacion" + "\"" + strSep +
+                        "\"" + "Responsable" + "\"" + strSep +
+                        "\"" + "Proyecto" + "\"" + strSep +
+                        "\"" + "Observaciones" + "\"" + strSep +
+                        "\"" + "Estado" + "\"" + strSep +
+                        "\"" + "Fecha Asignación" + "\"" + strSep +
+                        "\"" + "Fecha Vencimiento" + "\"" + strSep +
+                        "\"" + "Evaluadores" + "\"" + strSep +
+                        "\"" + "Calificación" + "\"" + strSep +
+                        "\"" + "Última Modificación" + "\"");
+                surveyByProvider.All(x =>
+                {
+                    if (x != null && x.SurveyInfo.Count > 0)
+                    {
+                        data.AppendLine
+                        (
+                        "\"" + x.RelatedSurveyConfig.ItemName.ToString() + "\"" + strSep +
+                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.Responsible).Select(a => a.Value).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.Project).Select(a => a.ValueName).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.Comments).Select(a => a.Value).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.Status).Select(a => a.ValueName).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.StartDate).Select(a => a.Value).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.EndDate).Select(a => a.Value).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                        "\"" + "N/D" + "\"" + strSep +
+                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.Rating).Select(a => a.Value).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                        "\"" + "N/D" + "\""
+                        );
+                        if (x.ChildSurvey != null && x.ChildSurvey.Count > 0)
+                        {
+                            x.ChildSurvey.All(y =>
+                            {
+                                if (y != null && y.ParentSurveyPublicId == x.SurveyPublicId)
+                                {
+                                    data.AppendLine
+                                      (
+                                        "\"" + x.RelatedSurveyConfig.ItemName.ToString() + "\"" + strSep +
+                                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.Responsible).Select(a => a.Value).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.Project).Select(a => a.ValueName).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                                        "\"" + y.SurveyInfo.Where(b => b.ItemInfoType.ItemId == (int)enumSurveyInfoType.Comments).Select(b => b.Value).DefaultIfEmpty("").FirstOrDefault() + "\"" + strSep +
+                                        "\"" + y.SurveyInfo.Where(b => b.ItemInfoType.ItemId == (int)enumSurveyInfoType.Status).Select(b => b.ValueName).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.StartDate).Select(a => a.Value).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                                        "\"" + x.SurveyInfo.Where(a => a.ItemInfoType.ItemId == (int)enumSurveyInfoType.EndDate).Select(a => a.Value).DefaultIfEmpty("").FirstOrDefault().ToString() + "\"" + strSep +
+                                       "\"" + y.User.ToString() + "\"" + strSep +
+                                       "\"" + y.SurveyInfo.Where(b => b.ItemInfoType.ItemId == (int)enumSurveyInfoType.Rating).Select(b => b.Value).DefaultIfEmpty("").FirstOrDefault() + "\"" + strSep +
+                                       "\"" + y.LastModify.ToString("dd/MM/yyyy") + "\""
+                                      );
+                                }
+                                return true;
+                            });
+                        }
+                    }
+                    return true;
+                });
+                buffer = Encoding.ASCII.GetBytes(data.ToString().ToCharArray());
+                return File(buffer, "application/csv", "InformacionGeneral_" + DateTime.Now.ToString("yyyyMMddHHmm") + ".csv");
+            }       
             return View(oModel);
         }
 
