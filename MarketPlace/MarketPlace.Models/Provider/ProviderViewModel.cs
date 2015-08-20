ba70d845a5cdcc585a6d18407b633d1fca0e6cec
José Antonio Forero Vargas <josexandthecity@outlook.com>
2015-08-20 11:15:09 -0500

    Se finaliza la carga de datos para Reporte de ED

--- MarketPlace/MarketPlace.Models/Provider/ProviderViewModel.cs
+++ MarketPlace/MarketPlace.Models/Provider/ProviderViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarketPlace.Models.General;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,8 @@ namespace MarketPlace.Models.Provider
         public ProveedoresOnLine.ProjectModule.Models.ProjectModel RelatedProject { get; set; }
 
         public ProviderLiteViewModel RelatedLiteProvider { get; set; }
+
+        public GenericReportModel SurveytReportModel { get; set; }
 
         public List<MarketPlace.Models.General.GenericMenu> ProviderMenu { get; set; }
 
