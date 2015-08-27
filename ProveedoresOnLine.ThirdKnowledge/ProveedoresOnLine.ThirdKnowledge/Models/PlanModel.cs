c251c16acba6bc3b881b72a4746e1f12603b80cd
SebasDeveloper <johann.martinez@carvajal.com>
2015-08-26 19:29:19 -0500

    Configuración del modulo de conocimiento de terceros

---
+++ ProveedoresOnLine.ThirdKnowledge/ProveedoresOnLine.ThirdKnowledge/Models/PlanModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.ThirdKnowledge.Models
+{
+    public class PlanModel
+    {
+        public string PlanPublicId { get; set; }
+
+        public string CompanyPublicId { get; set; }
+
+        public int QueriesByPeriod { get; set; }
+
+        public DateTime InitDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public CatalogModel Status { get; set; }
+
+        public int DaysByPeriod { get; set; }
+
+        public bool Enable { get; set; }
+
+        public DateTime LastModify { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public List<PeriodModel> RelatedPeriodoModel { get; set; }
+    }
+}
