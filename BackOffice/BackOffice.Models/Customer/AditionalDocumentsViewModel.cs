d6c64d6dbbe07ffc1772b2211a5a93cfa16e3088
David Moncayo <d.alonsomt@gmail.com>
2015-09-02 08:49:26 -0500

    se agrega el view model de customer

---
+++ BackOffice/BackOffice.Models/Customer/AditionalDocumentsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackOffice.Models.Customer
+{
+    public class AditionalDocumentsViewModel
+    {
+        public string AditionalDataId { get; set; }
+
+        public string AditionalDataTypeId { get; set; }
+
+        public string AditionalDataType { get; set; }
+
+        public string Title { get; set; }
+
+        public bool RenderScripts { get; set; }
+
+        public bool Enable { get; set; }
+
+        public AditionalDocumentsViewModel() { }
+
+        public AditionalDocumentsViewModel(ProveedoresOnLine.Company.Models.Util.GenericItemModel oAditionalData)
+        {
+            AditionalDataId = oAditionalData.ItemId.ToString();
+            AditionalDataTypeId = oAditionalData.ItemType.ItemId.ToString();
+            AditionalDataType = oAditionalData.ItemType.ItemName;
+            Title = oAditionalData.ItemInfo.Where(x => x.Value != null).Select(x => x.Value).DefaultIfEmpty(string.Empty).FirstOrDefault();
+        }
+    }
+}
