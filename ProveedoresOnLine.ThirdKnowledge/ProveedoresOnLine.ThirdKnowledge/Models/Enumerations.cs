b4b1f3488832669a627f42922caf78e152b7f4c5
SebasDeveloper <johann.martinez@carvajal.com>
2015-09-09 11:02:26 -0500

    Enumeraciones Excluidas

---
+++ ProveedoresOnLine.ThirdKnowledge/ProveedoresOnLine.ThirdKnowledge/Models/Enumerations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.ThirdKnowledge.Models
+{
+    public class Enumerations
+    {
+        public enum enumThirdKnowledgeColls
+        {
+            RequestName = 301001,
+            IdNumberRequest = 301002,
+            QueryId = 301003,
+            GroupNumber = 301004,
+            GroupName = 301005,
+            Priotity = 301006,
+            TypeDocument = 301007,
+            IdNumberResult = 301008,
+            NameResult = 301009,
+            IdList = 301010,
+            ListName = 301011,
+            Alias = 301012,
+            Offense = 301013,
+            Peps = 301014,
+            Status = 301015,
+            Zone = 301016,
+            Link = 301017,
+            MoreInfo = 301018,
+            RegisterDate = 301019,
+            LastModifyDate = 301020,
+            Message = 301021,
+        }
+    }
+}
