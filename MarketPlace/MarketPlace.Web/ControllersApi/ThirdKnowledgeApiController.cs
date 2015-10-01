312fc8eb674be9c5c71df99eba86e18d8e1b6e18
José Antonio Forero Vargas <josexandthecity@outlook.com>
2015-09-30 19:04:55 -0500

    Se mueven las funciones de envio de correo para notificar la carga de archivos a la dll de Conocimiento de terceros.

--- MarketPlace/MarketPlace.Web/ControllersApi/ThirdKnowledgeApiController.cs
+++ MarketPlace/MarketPlace.Web/ControllersApi/ThirdKnowledgeApiController.cs
@@ -167,10 +167,16 @@ namespace MarketPlace.Web.ControllersApi
 
                         oQueryToCreate = ProveedoresOnLine.ThirdKnowledge.Controller.ThirdKnowledgeModule.QueryUpsert(oQueryToCreate);
 
-                        // Send Mail
-                        MessageModule.Client.Models.ClientMessageModel oMessageToSend = new MessageModule.Client.Models.ClientMessageModel();
-                        oMessageToSend = GetUploadSuccessFileMessage();
-                        MessageModule.Client.Controller.ClientController.CreateMessage(oMessageToSend);
+                        //Send Message
+                        MessageModule.Client.Models.NotificationModel oDataMessage = new MessageModule.Client.Models.NotificationModel();
+                        oDataMessage.CompanyPublicId = CompanyPublicId;
+                        oDataMessage.User = SessionModel.CurrentLoginUser.Email;
+                        oDataMessage.CompanyLogo = SessionModel.CurrentCompany_CompanyLogo;
+                        oDataMessage.CompanyName = SessionModel.CurrentCompany.CompanyName;
+                        oDataMessage.IdentificationType = SessionModel.CurrentCompany.IdentificationType.ItemName;
+                        oDataMessage.IdentificationNumber = SessionModel.CurrentCompany.IdentificationNumber;
+
+                        ProveedoresOnLine.ThirdKnowledge.Controller.ThirdKnowledgeModule.CreateUploadNotification(oDataMessage);
 
                         MessageModule.Client.Models.NotificationModel oNotification = new MessageModule.Client.Models.NotificationModel()
                         {
@@ -284,36 +290,7 @@ namespace MarketPlace.Web.ControllersApi
                 }
             }
             return false;
-        }
-
-        private MessageModule.Client.Models.ClientMessageModel GetUploadSuccessFileMessage()
-        {
-            //Create message object
-            MessageModule.Client.Models.ClientMessageModel oReturn = new MessageModule.Client.Models.ClientMessageModel()
-            {
-                Agent = Models.General.InternalSettings.Instance[Models.General.Constants.C_Settings_TK_UploadSuccessFileAgent].Value,
-                User = SessionModel.CurrentLoginUser.Email,
-                ProgramTime = DateTime.Now,
-                MessageQueueInfo = new List<Tuple<string, string>>(),
-            };
-
-            oReturn.MessageQueueInfo.Add(new Tuple<string, string>("To", SessionModel.CurrentLoginUser.Email));
-
-            //get customer info
-            oReturn.MessageQueueInfo.Add(new Tuple<string, string>
-                ("CustomerLogo", SessionModel.CurrentCompany_CompanyLogo));
-
-            oReturn.MessageQueueInfo.Add(new Tuple<string, string>
-                ("CustomerName", SessionModel.CurrentCompany.CompanyName));
-
-            oReturn.MessageQueueInfo.Add(new Tuple<string, string>
-                ("CustomerIdentificationTypeName", SessionModel.CurrentCompany.IdentificationType.ItemName));
-
-            oReturn.MessageQueueInfo.Add(new Tuple<string, string>
-                ("CustomerIdentificationNumber", SessionModel.CurrentCompany.IdentificationNumber));
-
-            return oReturn;
-        }
+        }    
 
         #endregion Private Functions
     }
