dc3e17052b018e3f7ee9db01093c0afbf2e273f1
SebasDeveloper <johann.martinez@carvajal.com>
2016-01-17 14:41:25 -0500

    Modelos de homologacion

---
+++ ProveedoresOnLine.AsociateProvider/ProveedoresOnLine.AsociateProvider.Client/Models/CatalogModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.AsociateProvider.Client.Models
+{
+    public class CatalogModel
+    {
+        public int CatalogId { get; set; }
+
+        public string CatalogName { get; set; }
+
+        public int ItemId { get; set; }
+
+        public string ItemName { get; set; }
+    }
+}
---
+++ ProveedoresOnLine.AsociateProvider/ProveedoresOnLine.AsociateProvider.Client/Models/HomologateModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.AsociateProvider.Client.Models
+{
+    public class HomologateModel
+    {
+        public int HologateId { get; set; }
+
+        public  CatalogModel HomologateType { get; set; }
+
+        public CatalogModel Source { get; set; }
+
+        public CatalogModel HomologateType { get; set; }
+
+        public DateTime LastModify { get; set; }
+
+        public DateTime CreateDate { get; set; }
+    }
+}
