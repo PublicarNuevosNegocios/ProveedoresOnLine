f162a471d34538f93252b839766c3a7c707588b4
SebasDeveloper <johann.martinez@carvajal.com>
2016-02-26 10:19:29 -0500

    Test al proceso de batch

---
+++ ProveedoresOnLine.RestrictiveListProcess/ProveedoresOnLine.RestrictiveListProcess.Test/RestrictiveListProcessBatchTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProveedoresOnLine.RestrictiveListProcess.Test
+{
+    [TestClass]
+    public class RestrictiveListProcessBatchTest
+    {
+        [TestMethod]
+        public void StartProcessTest()
+        {
+            ProveedoresOnLine.RestrictiveListProcessBatch.RestrictiveListSendProcess.StartProcess();
+        }
+    }
+}
