6616cf8ba83b69b011d4bbcc86535ac51f5c0065
SebasDeveloper <johann.martinez@carvajal.com>
2016-03-14 14:19:05 -0500

    Implementación del proceso de black list

---
+++ ProveedoresOnLine.Process/ProveedoresOnLine.Process.Test/RestrictiveListSendProcessJobTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProveedoresOnLine.Process.Test
+{
+    [TestClass]
+    public class RestrictiveListSendProcessJobTest
+    {
+        [TestMethod]
+        public void RestrictiveListSendProcessJob_Execute()
+        {
+            ProveedoresOnLine.Process.Implement.RestrictiveListSendProcess SBJOb = new Implement.RestrictiveListSendProcess();
+            SBJOb.Execute(null);
+        }
+    }
+}
