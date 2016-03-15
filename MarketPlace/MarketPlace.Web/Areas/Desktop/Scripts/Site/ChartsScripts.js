bc58afca073070f4fee1763a48c17e7639b1a8f9
Alexander JP <gustavo.jurado@publicar.com>
2016-03-15 09:59:51 -0500

    resize  dash board charts

--- MarketPlace/MarketPlace.Web/Areas/Desktop/Scripts/Site/ChartsScripts.js
+++ MarketPlace/MarketPlace.Web/Areas/Desktop/Scripts/Site/ChartsScripts.js
@@ -35,7 +35,7 @@ var Survey_ChartsObject = {
                     'options': {
                         colors: ['#FF6961', '#77DD77', '#966FD6', '#FDFD96', '#FFD1DC', '#03C03C', '#779ECB', '#C23B22'],
                         is3D: true,
-                        chartArea: { left: 100, top: 5, width: "80%", height: "90%" },
+                        chartArea: { left: 90, top: 0, width: "88%", height: "98%" },
                         height: "100%",
                         width: "100%"
                     },
