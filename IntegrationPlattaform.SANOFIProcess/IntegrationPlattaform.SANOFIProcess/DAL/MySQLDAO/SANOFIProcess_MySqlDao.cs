8c570d98fbe61218b16038ab37e86c4036e8272b
SebasDeveloper <johann.martinez@carvajal.com>
2016-05-26 11:22:29 -0500

    SANOFI PROCESS

---
+++ IntegrationPlattaform.SANOFIProcess/IntegrationPlattaform.SANOFIProcess/DAL/MySQLDAO/SANOFIProcess_MySqlDao.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationPlattaform.SANOFIProcess.DAL.MySQLDAO
+{
+    internal class SANOFIProcess_MySqlDao
+    {
+    }
+}
