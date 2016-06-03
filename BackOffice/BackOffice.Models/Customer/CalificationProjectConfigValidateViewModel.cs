a69d72a344defc6ce39258d38387eb76d5b3f9e6
Diego F Jaramillo <diego.jaramillo@publicar.com>
2016-06-03 14:17:57 -0500

    Calification Project Config Validation, View and API

--- BackOffice/BackOffice.Models/Customer/CalificationProjectConfigValidateViewModel.cs
+++ BackOffice/BackOffice.Models/Customer/CalificationProjectConfigValidateViewModel.cs
@@ -9,9 +9,9 @@ namespace BackOffice.Models.Customer
     public class CalificationProjectConfigValidateViewModel
     {
         public ProveedoresOnLine.CalificationProject.Models.CalificationProject.ConfigValidateModel RelatedCalificationProjectConfigValidateModel { get; set; }
-        public int CalificationProjectConfigValidateId { get; set; }
-        public int CalificationProjectConfigId { get; set; }
-        public string Operator { get; set; }
+        public string CalificationProjectConfigValidateId { get; set; }
+        public string CalificationProjectConfigId { get; set; }
+        public int Operator { get; set; }
         public string Value { get; set; }
         public string Result { get; set; }
         public bool Enable { get; set; }
@@ -20,9 +20,9 @@ namespace BackOffice.Models.Customer
         public CalificationProjectConfigValidateViewModel(ProveedoresOnLine.CalificationProject.Models.CalificationProject.ConfigValidateModel oRelatedCalificationProjectConfigValidateModel) 
         {
             RelatedCalificationProjectConfigValidateModel = oRelatedCalificationProjectConfigValidateModel;
-            CalificationProjectConfigId = RelatedCalificationProjectConfigValidateModel.CalificationProjectConfigId;
-            CalificationProjectConfigValidateId = RelatedCalificationProjectConfigValidateModel.CalificationProjectConfigValidateId;
-            Operator = RelatedCalificationProjectConfigValidateModel.Operator.ItemId.ToString();
+            CalificationProjectConfigId = RelatedCalificationProjectConfigValidateModel.CalificationProjectConfigId.ToString();
+            CalificationProjectConfigValidateId = RelatedCalificationProjectConfigValidateModel.CalificationProjectConfigValidateId.ToString();
+            Operator = RelatedCalificationProjectConfigValidateModel.Operator.ItemId;
             Value = RelatedCalificationProjectConfigValidateModel.Value.ToString();
             Result = RelatedCalificationProjectConfigValidateModel.Result.ToString();
             Enable = RelatedCalificationProjectConfigValidateModel.Enable;
