3b9018ca47e5bc2a1ccdda5dedff3fc00e2c2a1c
Diego F Jaramillo <diego.jaramillo@publicar.com>
2016-06-08 17:10:31 -0500

    Creación model SanofiGeneralInfo

--- IntegrationPlattaform.SANOFIProcess/IntegrationPlattaform.SANOFIProcess/Interfaces/IIntegrationPlatformSANOFIProcessData.cs
+++ IntegrationPlattaform.SANOFIProcess/IntegrationPlattaform.SANOFIProcess/Interfaces/IIntegrationPlatformSANOFIProcessData.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IntegrationPlattaform.SANOFIProcess.Models;
 
 namespace IntegrationPlattaform.SANOFIProcess.Interfaces
 {
     interface IIntegrationPlatformSANOFIProcessData
     {
+        
     }
 }
