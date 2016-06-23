c207bfc345abcd6d05211effc4025ed34b4631f4
David Moncayo <d.alonsomt@gmail.com>
2016-06-23 08:15:34 -0500

    se agrega el método de ejecución del proceso de calificación a la dll de process.

---
+++ ProveedoresOnLine.Process/ProveedoresOnLine.Process.Test/CalificationProjectProcessJobTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProveedoresOnLine.Process.Test
+{
+    [TestClass]
+    public class CalificationProjectProcessJobTest
+    {
+        [TestMethod]
+        public void CalificationProjectProcessJob()
+        {
+            ProveedoresOnLine.Process.Implement.CalificationProjectProcessJob CalificationProjectBatch = new Implement.CalificationProjectProcessJob();
+
+            CalificationProjectBatch.Execute(null);
+        }
+    }
+}
