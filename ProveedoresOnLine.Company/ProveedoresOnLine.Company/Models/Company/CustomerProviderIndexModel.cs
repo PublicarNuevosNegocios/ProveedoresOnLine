2c54b490e5d1dd4afee328bcad383f2dfde08301
David Moncayo <d.alonsomt@gmail.com>
2016-09-06 16:32:08 -0500

    se agrega modelo customerproviderindexmodel.

---
+++ ProveedoresOnLine.Company/ProveedoresOnLine.Company/Models/Company/CustomerProviderIndexModel.cs
@@ -0,0 +1,32 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProveedoresOnLine.Company.Models.Company
+{
+    [ElasticsearchType(Name = "CustomerProvider_Info")]
+    public class CustomerProviderIndexModel
+    {
+        public CustomerProviderIndexModel()
+        {
+
+        }
+
+        public int Id { get { return CustomerProviderId; } }
+
+        public int CustomerProviderId { get; set; }
+
+        public string CustomerPublicId { get; set; }
+
+        public string ProviderPublicId { get; set; }
+
+        public int StatusId { get; set; }
+
+        public string Status { get; set; }
+
+        public bool CustomerProviderEnable { get; set; }
+    }
+}
